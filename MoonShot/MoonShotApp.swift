3efb5ca8e79e11870c2deefcc3536a012463e48a
David Olagookun <erijesudo@gmail.com>
2023-03-14 04:49:23 +0100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by David OH on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
